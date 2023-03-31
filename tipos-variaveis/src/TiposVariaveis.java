742bf5e1a372692cd01ef3d2ffe031e66de2f527
aurelio-dasilva <aurelio-dasilva@outlook.com>
2023-03-31 17:55:04 -0300

    tipos e metodos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int numero = -5;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        System.out.println(numero);
     }
 }
