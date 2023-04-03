399c0ade4344213469890ee15c3a085dbd360a32
aurelio-dasilva <aurelio-dasilva@outlook.com>
2023-04-03 19:12:05 -0300

    Exercicio simulando smartTv

---
+++ smart-tv/src/SmartTv.java
@@ -0,0 +1,40 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligarTv(){
+        ligada= true;
+        System.out.println("Tv ligada? " + ligada);
+    }
+
+    public void desligarTv(){
+        ligada= false;
+        System.out.println("Tv ligada? " + ligada);
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Volume atual: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Volume atual: " + volume);
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+        System.out.println("canal atual: "+ canal);
+    }
+
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("canal atual: "+ canal);
+    }
+
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("canal atual: "+ canal);
+}
+}
---
+++ smart-tv/src/Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario {
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.ligarTv();
+        smartTv.desligarTv();
+        smartTv.mudarCanal(10);
+        smartTv.aumentarCanal();
+        smartTv.aumentarVolume();
+    }
+}
